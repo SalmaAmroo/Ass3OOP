2540619b7f79b40c118be4efcb993e47b3b0ce6e
Admin <Admin@SALMAAMRO>
2025-07-27 17:32:08 +0300

    Q1

--- Ass3OOP/Program.cs
+++ Ass3OOP/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿namespace Ass3OOP
+{
+
+    #region Q1
+    //class Calculator 
+    //{
+    //    public int Add (int a , int b)
+    //    {
+    //        return a + b;
+    //    }
+    //    public int Add(int a, int b,int c)
+    //    {
+    //        return a + b +c;
+    //    }
+    //    public double Add(double a, double b)
+    //    {
+    //        return a + b;
+    //    }
+
+    //}
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region CallQ1
+            //Calculator calculator = new Calculator();
+            //Console.WriteLine(calculator.Add(1, 2));
+            //Console.WriteLine(calculator.Add(1, 2, 3));
+            //Console.WriteLine(calculator.Add(1.4, 2.5));
+
+            #endregion
+
+        }
+    }
+}
