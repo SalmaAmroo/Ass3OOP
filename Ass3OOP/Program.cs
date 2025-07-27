580aa4cbd5c9a9df4aa32f2e0c6b33635bbb7845
Admin <Admin@SALMAAMRO>
2025-07-27 17:43:18 +0300

    Q2

--- Ass3OOP/Program.cs
+++ Ass3OOP/Program.cs
@@ -19,6 +19,33 @@
 
     //}
     #endregion
+
+    #region Q2
+    //class Regtangle
+    //{
+    //     int height, widht;
+    //    public Regtangle() 
+    //    {
+    //      height = widht = 0;
+    //    }
+    //    public Regtangle(int height,int widht)
+    //    {
+    //        this.height = height;
+    //        this.widht = widht;
+    //    }
+    //    public Regtangle(int value)
+    //    {
+    //        height = widht = value;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        return $"height = {height} , widht = {widht}";
+    //    }
+
+
+    //}
+    #endregion
     class Program
     {
         static void Main(string[] args)
@@ -28,6 +55,17 @@
             //Console.WriteLine(calculator.Add(1, 2));
             //Console.WriteLine(calculator.Add(1, 2, 3));
             //Console.WriteLine(calculator.Add(1.4, 2.5));
+
+            #endregion
+
+            #region CallQ2
+            //Regtangle regtangle  = new Regtangle();
+            //Regtangle regtangle1 = new Regtangle(10, 20);
+            //Regtangle regtangle2 = new Regtangle(10);
+            //Console.WriteLine(regtangle);
+            //Console.WriteLine(regtangle1);
+            //Console.WriteLine(regtangle2);
+
 
             #endregion
 
